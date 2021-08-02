eb35930f3de7d4c6ef0c0a8c5c5f258663c11711
devnydv <80939011+devnydv@users.noreply.github.com>
2021-08-02 20:27:00 +0530

    Add files via upload

---
+++ ama.js
@@ -0,0 +1,208 @@
+function setama(){
+  localStorage.setItem("nextp","ama")
+};
+function setflip(){
+  localStorage.setItem("nextp","flip")
+};
+function setmyn(){
+  localStorage.setItem("nextp","myn")
+};
+function setajio(){
+  localStorage.setItem("nextp","ajio")
+};
+
+
+
+
+firebase.database().ref('newlist').
+  on('value', function (snapshot) {
+    let data = snapshot.val()
+    let key = Object.keys(data);
+    //console.log(data)
+
+    for (let i = 0; i < key.length; i++) {
+
+
+
+
+
+      firebase.database().ref('newlist/' + key[i]).
+        once('value', function (snapshot) {
+          let fd = snapshot.val();
+
+          
+          
+
+
+          let elem = document.getElementById('pate');
+          let a = document.createElement('a');
+          a.setAttribute('href', fd.link);
+          let mydiv = document.createElement('div')
+
+          let img = document.createElement('img')
+
+          img.setAttribute('src', fd.image);
+          img.setAttribute('alt', "img");
+          mydiv.appendChild(img);
+          let nam = document.createElement('p');
+          nam.innerText = key[i];
+          mydiv.appendChild(nam)
+          let p = document.createElement('p')
+          p.innerText = fd.price
+          mydiv.appendChild(p)
+
+          elem.appendChild(a);
+          a.appendChild(mydiv);
+          //console.log(elem)
+
+        })
+
+
+    }
+
+  });
+
+
+
+
+
+
+
+let val;
+
+
+//function ama(btm) {
+  val = localStorage.getItem("nextp");
+  let mbut = document.getElementById("but")
+  mbut.remove();
+
+
+
+  firebase.database().ref(val).
+    on('value', function (snapshot) {
+      let data = snapshot.val()
+      let key = Object.keys(data);
+
+
+      let fa = document.getElementById('page-content');
+      let newdiv = document.createElement('div');
+      newdiv.setAttribute('id', 'but');
+
+
+      for (let i = 0; i < key.length; i++) {
+
+
+
+        firebase.database().ref(`${val}/` + key[i]).
+          once('value', function (snapshot) {
+            let fd = snapshot.val();
+
+            //keyw = Object.keys(fd)
+
+
+
+            //  let butelem = document.getElementById('but');
+            let newbut = document.createElement('button');
+            newbut.setAttribute('value', key[i])
+            newbut.setAttribute('onclick', 'me(this)')
+            newbut.setAttribute('id', 'mybut')
+            newbut.innerText = key[i];
+            newdiv.appendChild(newbut);
+            let pos = fa.firstElementChild.nextElementSibling.nextElementSibling;
+            fa.insertBefore(newdiv, pos);
+            // console.log(newdiv)
+
+
+          })
+
+      }
+      let load = document.getElementById('p2');
+      load.remove();
+    })
+
+// }
+
+
+
+
+
+function me(btm) {
+  val2 = btm.value
+
+  // console.log(val2)
+
+let topp =document.getElementById('top')
+topp.innerHTML=val2;
+
+
+
+  let rem = document.getElementById('pate');
+  rem.remove();
+  //  let fdiv = document.getElementById('fload');
+  //  let load = document.createElement('div');
+  //  load.setAttribute('id', 'loader');
+  //  fload.appendChild(load);
+
+  let fa = document.getElementById('bigdiv');
+  let newdiv = document.createElement('div');
+  newdiv.setAttribute('id', 'pate');
+  fa.appendChild(newdiv);
+
+  //console.log(keyw)
+
+
+        firebase.database().ref(`${val}/` +`${val2}/`).
+          once('value', function (snapshot) {
+            let fd = snapshot.val();
+
+            let keyw = Object.keys(fd)
+
+          
+            //console.log(val)
+            // console.log(val2)
+
+
+            for (let i = 0; i < keyw.length; i++) {
+
+              firebase.database().ref(`${val}/` + `${val2}/` + keyw[i]).
+                once('value', function (snapshot) {
+                  let fdd = snapshot.val();
+
+
+
+                  let elem = document.getElementById('pate');
+
+                  let a = document.createElement('a');
+                  a.setAttribute('href', fdd.link);
+                  a.setAttribute('target', "_blank");
+                  a.setAttribute('rel', "nofollow");
+                  let mydiv = document.createElement('div')
+
+                  let img = document.createElement('img')
+
+                  img.setAttribute('src', fdd.image);
+                  mydiv.appendChild(img);
+                  let nam = document.createElement('p');
+                  nam.innerText = keyw[i];
+                  mydiv.appendChild(nam)
+                  let p = document.createElement('p')
+                  p.innerText = fdd.price
+                  mydiv.appendChild(p)
+
+                  elem.appendChild(a);
+                  a.appendChild(mydiv);
+                  //console.log(elem)
+                  
+
+                })
+              }
+              
+      })
+      
+
+
+
+    
+
+
+  }
